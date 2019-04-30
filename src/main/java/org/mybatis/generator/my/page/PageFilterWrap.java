b99f9cb927943ef17f0f0de6e493bfaedc26f30c
hugo <zxh01@jumapeisong.com>
2019-04-30 10:07:54 +0800

    公共服务

---
+++ src/main/java/org/mybatis/generator/my/page/PageFilterWrap.java
@@ -0,0 +1,66 @@
+package org.mybatis.generator.my.page;
+
+import java.io.Serializable;
+
+public class PageFilterWrap<T> implements Serializable {
+
+    /**
+     * serialVersionUID
+     */
+    private static final long serialVersionUID = -2792346671122455743L;
+
+    private Integer pageNo = 1;
+
+    private Integer pageSize = 15;
+
+    private String orderBy;
+
+    private String orderSort;
+
+    private T filters;
+
+    public Integer getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(Integer pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public String getOrderSort() {
+        return orderSort;
+    }
+
+    public void setOrderSort(String orderSort) {
+        this.orderSort = orderSort;
+    }
+
+    public T getFilters() {
+        return filters;
+    }
+
+    public void setFilters(T filters) {
+        this.filters = filters;
+    }
+
+    public Integer getStartOffset() {
+        return (pageNo - 1) * pageSize;
+    }
+
+}
--- src/main/java/org/mybatis/generator/my/service/AbstractService.java
+++ src/main/java/org/mybatis/generator/my/service/AbstractService.java
@@ -3,6 +3,8 @@ package org.mybatis.generator.my.service;
 import java.util.List;
 
 import org.mybatis.generator.my.mapper.Mapper;
+import org.mybatis.generator.my.page.Page;
+import org.mybatis.generator.my.page.PageQueryParamWrap;
 
 /**
  * 
@@ -18,6 +20,19 @@ import org.mybatis.generator.my.mapper.Mapper;
 public abstract class AbstractService<En, Ex> {
 
     abstract Mapper<En, Ex> getMapper();
+
+    abstract <T> Ex pageOfExample(PageQueryParamWrap<T> query);
+
+    <T> Page<En> pageOf(PageQueryParamWrap<T> query) {
+        Page<En> p = new Page<En>();
+        Ex ex = pageOfExample(query);
+        int total = (int) countByExample(ex);
+        p.setTotal(total);
+        if (total != 0) {
+            p.setResults(selectByExample(ex));
+        }
+        return p;
+    }
 
     long countByExample(Ex example) {
         return getMapper().countByExample(example);
